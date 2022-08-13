0a120b8d428040b289b78a55048d8947f3e37c64
binyang <bybookcover@hotmail.com>
2022-08-13 11:17:11 +0800

    02

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
+
+app.post('/',(req,res)=>{
+    res.send('Post/!')
+})
+
 app.listen(3000,()=>{
     console.log('Server running at http://localhost:3000/')
 })
