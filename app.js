fb44802231455c79b5789bcc2459a17b988f71bb
binyang <bybookcover@hotmail.com>
2022-08-15 16:14:57 +0800

    路由设置

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!')
+    res.send('get /todos')
 })
 
-app.post('/',(req,res)=>{
-    res.send('Post/!')
+app.get('/todos',(req,res)=>{
+    res.send('get /todos')
+})
+
+app.get('/todos/:id',(req,res)=>{
+    res.send(`get /todos/${req.params.id}`)
+})
+
+app.post('/todos',(req,res)=>{
+    res.send('post /todos')
+})
+
+app.patch('/todos/:id',(req,res)=>{
+    res.send('patch /todos')
+})
+app.delete('/todos/:id',(req,res)=>{
+    res.send(`delete /todos/${req.params.id}`)
 })
 
 app.listen(3000,()=>{
